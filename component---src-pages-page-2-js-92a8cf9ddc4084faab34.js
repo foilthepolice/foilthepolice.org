4cf3bc47cd95aa9cbaffdbf6985aca02788ee642
Mark Hansen <x@markthemark.com>
2020-06-14 15:39:17 -0400

    Updates

--- component---src-pages-page-2-js-92a8cf9ddc4084faab34.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("pJH7"),p=a("hIod");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(p.a,{title:"Home"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-92a8cf9ddc4084faab34.js.map
