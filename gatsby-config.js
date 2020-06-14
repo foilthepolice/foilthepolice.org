f81e4e349e7f47a66e86349cebfe8b639f020ac3
Mark Hansen <x@markthemark.com>
2020-06-14 16:01:02 -0400

    refactor: remove path prefix now that we're serving from a new non-github root

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `FOIL the police`,
-    description: `TODO(nsahler)`,
-    author: `@gatsbyjs`,
+    title: `Foil the Police`,
+    description: `Freedom of Information (FOI) requests are a tool everyone has to uncover police department lies and violent officers. E-mail your local government clerk with a request template and see what you uncover.`,
   },
-  pathPrefix: "/foilthepolice.org",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
