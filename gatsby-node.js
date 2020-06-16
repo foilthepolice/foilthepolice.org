10687e7d2bdf1fe18b38a9662017fa13d9142b0a
Mark Hansen <x@markthemark.com>
2020-06-15 22:01:29 -0400

    fix: gatsby-node jurisdictions not being used

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,3 @@
-const JURISDICTIONS = require('./data')
-const path = require(`path`)
-
-/* TODO(nsahler): Use fs and basic MD and JSON files instead */
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  const stateTemplate = path.resolve(`./src/templates/jurisdiction.js`)
-  console.log(JURISDICTIONS)
-  for (var j of JURISDICTIONS) {
-    createPage({
-      // Path for this page — required
-      path: `${j.slug}`,
-      component: stateTemplate,
-      context: j,
-    }) 
-  }
+  // Clearing until we do state pages again
 }
