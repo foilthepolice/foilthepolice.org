6e79c9ee82fa062ae21c01b31296ed7ae829b3b5
Mark Hansen <x@markthemark.com>
2020-06-20 11:34:10 -0400

    copy: . > ,

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const FooterWrapper = styled.div`
 const Footer = ({ children }) => {
   return (
     <FooterWrapper>
-      <H3>No Justice. No Peace.</H3>
+      <H3>No Justice, No Peace.</H3>
       <small><A href="mailto:contact@foilthepolice.org">contact@foilthepolice.org</A></small>
     </FooterWrapper>
   )
