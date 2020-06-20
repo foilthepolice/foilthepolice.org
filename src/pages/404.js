bb06c95d137755eaf56513becf3f45051b5e5dc3
Mark Hansen <x@markthemark.com>
2020-06-20 11:57:04 -0400

    fix: missing section styling/component on error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,28 @@
-import React from "react"
+import React from 'react';
+import styled from 'styled';
 
-import Scaffolding from "../components/Scaffolding"
+import Scaffolding from '../components/Scaffolding'
 import { A, H1, P } from '../components/Typography';
+
+const ErrorSection = styled.div`
+  padding-bottom: 2em;
+  margin-bottom: 2em;
+  &:last-of-type {
+    margin-bottom: 6em;
+  }
+  h1 {
+    padding: 1em 0;
+  }
+`;
 
 const NotFoundPage = () => (
   <Scaffolding>
-    <IndexSection>
+    <ErrorSection>
       <H1>F.O.I.L. The Police</H1>
       <P>
         There doesn't seem to be a page here. <A href="/">Go to homepage</A>
       </P>
-    </IndexSection>
+    </ErrorSection>
   </Scaffolding>
 )
 
