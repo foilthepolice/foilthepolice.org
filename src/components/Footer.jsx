8237c50758afb6c5e65cdb3325c8557c2df544a2
Mark Hansen <x@markthemark.com>
2020-06-20 13:17:33 -0400

    improvement: footer text to show you don't need to be a professional to do this

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../constants';
-import { A, H3 } from './Typography';
+import { A, H3, P } from './Typography';
 
 const FooterWrapper = styled.div`
   padding: 2em;
@@ -15,6 +15,8 @@ const Footer = ({ children }) => {
   return (
     <FooterWrapper>
       <H3>No Justice, No Peace.</H3>
+      <P>This site started when 3 neighbors in a small town (3000 pop.) set out to hold their local police accountable.</P>
+      <P>You have the power to investigate your local police. Please share your progress with us.</P>
       <small><A href="mailto:contact@foilthepolice.org">contact@foilthepolice.org</A></small>
     </FooterWrapper>
   )
